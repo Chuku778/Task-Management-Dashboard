d2988df9082a6e29e57988d66cda3d3815c2e2b0
Chuku778 <joychioma777@gmail.com>
2026-04-10 09:50:43 +0100

    updated

--- src/component/ui/AddTask.jsx
+++ src/component/ui/AddTask.jsx
@@ -118,7 +118,7 @@ const AddTask = ({ onAdd, teamMembers = [] }) => {
 
       <button
         type="submit"
-        className="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 transition"
+        className="bg-green-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 transition"
       >
         Add Task
       </button>
